5d0c54e69cd66f1378df021f5922b7b904b2eeef
olphi <kirichgordeev@gmail.com>
2020-11-23 09:46:29 +0700

    halo

--- my first repo/first.py
+++ my first repo/first.py
@@ -1,1 +1,45 @@
-print('My first git-repo')
+import sys
+import random
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton
+from PyQt5.QtGui import QPainter, QColor
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.qp = QPainter()
+        self.status = 0
+        self.setMouseTracking(True)
+        self.btn = QPushButton('hehe', self)
+        self.btn.clicked.connect(self.hehe)
+
+    def hehe(self):
+        self.status = 1
+        self.update()
+
+    def paintEvent(self, event):
+        self.qp = QPainter()
+        self.qp.begin(self)
+        self.draw()
+        self.qp.end()
+
+    def draw(self):
+        self.qp.setBrush(QColor(random.randint(0, 255),
+                                random.randint(0, 255), random.randint(0, 255)))
+        if self.status == 1:
+            self.qp.drawEllipse(random.randint(10, 200), random.randint(
+                10, 200), random.randint(10, 200), random.randint(10, 200))
+        self.status = 0
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
